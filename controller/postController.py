05035b61b666f4162c2f50fc7886043ab4db29aa
Gautham <gauthamskaniyattil@gmail.com>
2025-10-01 14:02:53 +0530

    added create_post fn

--- controller/postController.py
+++ controller/postController.py
@@ -9,5 +9,9 @@ async def get_post_by_id(id : int):
     post = queryId(POSTS , id)
     return post
 
-# async def create_post(new_post : Post):
-
+async def create_post(new_post : Post):
+    new_id : int = len(POSTS)+1 if POSTS else 1
+    post_dict : dict = new_post.model_dump()
+    post_dict["id"] = new_id
+    POSTS.append(post_dict)
+    return post_dict
