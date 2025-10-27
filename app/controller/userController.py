fcc21de0bea8370f7953a71c43e5cd71aa322dee
Gautham <gauthamskaniyattil@gmail.com>
2025-10-27 17:20:26 +0530

    Added hasing

--- app/controller/userController.py
+++ app/controller/userController.py
@@ -2,11 +2,14 @@ from sqlalchemy.orm import Session
 import app.schema.userSchema as Schema
 import app.model as model
 from fastapi import HTTPException,status
+from app.utils import hashPassword
 
 async def create_users(user : Schema.CreateUser , db : Session):
         existing = db.query(model.User).filter(model.User.email == user.email).first()
         if existing is not None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User Already exists")
+        user.password = hashPassword(user.password)
+        print(f"Password before hashing {type(user.password)}")
         user_data = model.User(**user.model_dump())
         db.add(user_data)
         db.commit()
